7dc9d6e888884f4385e5d11a0ede0b8f9c9e65c1
Sahil Sonkamble <150232278+sonkamblesahil@users.noreply.github.com>
2026-02-04 00:05:04 +0530

    minor ui changes for better looks

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function DashBoardPage() {
   return (
-    <div className='bg-whiteh-full'>
-      DashBoard Page
+    <div className='bg-gray-400 h-full p-2'>
+      <h1 className="text-zinc-600 text-base font-bold">Welcome Sahil Sonkamble</h1>
+      <div className='h-[82vh] rounded-2xl flex items-center justify-center bg-white mt-2'>
+
+      </div>
     </div>
   )
 }
